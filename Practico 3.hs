ce93ab03caf1f36b1d328305f5b2feee2f4af375
Ivan Rodriguez <40413833+IvanRod1@users.noreply.github.com>
2023-04-06 20:33:37 -0300

    apto para corrección

--- Practico 3.hs
+++ Practico 3.hs
@@ -145,7 +145,6 @@ data Tree a = EmptyT | NodeT a (Tree a) (Tree a) deriving Show
 arbolNumeros :: Tree Int
 arbolNumeros = NodeT 4 (NodeT 8 (NodeT 2 EmptyT EmptyT ) EmptyT) EmptyT
 
-
 arbolEjemplo :: Tree Int
 arbolEjemplo = NodeT 9 (NodeT 10 (NodeT 20 (NodeT 50 EmptyT EmptyT) EmptyT) EmptyT) EmptyT
 
@@ -158,20 +157,18 @@ arbolConfuso = NodeT "Hola" (NodeT "Como" (NodeT "Buenas" EmptyT EmptyT) (NodeT 
 arbolEjemploPaint :: Tree Int
 arbolEjemploPaint = NodeT 9 (NodeT 10 (NodeT 20 (NodeT 50 EmptyT (NodeT 1 (NodeT 5 EmptyT EmptyT) (NodeT 3 EmptyT EmptyT))) EmptyT) EmptyT) EmptyT
 
-arbolEjemploPaint2 :: Tree Int
-arbolEjemploPaint2 = NodeT 9 
-                            (NodeT 10 
-                                (NodeT 20 
-                                     (NodeT 50 
-                                        EmptyT 
-                                            (NodeT 1 
-                                                (NodeT 5 EmptyT EmptyT) 
-                                                    (NodeT 3 EmptyT EmptyT))) EmptyT)
-                                                                                    EmptyT) 
-                                                                                         EmptyT
-
 arbolSinArboles :: Tree Int
 arbolSinArboles = NodeT 2 EmptyT EmptyT
+
+arbolFidel :: Tree Int
+arbolFidel = NodeT 1 (NodeT 2 (NodeT 4 (NodeT 8 EmptyT EmptyT)
+                          EmptyT)
+                 (NodeT 5 EmptyT
+                          EmptyT))
+        (NodeT 3 (NodeT 6 EmptyT
+                          (NodeT 9 EmptyT EmptyT))
+                 (NodeT 7 EmptyT
+                          EmptyT))
 
 --1
 sumarT :: Tree Int -> Int
@@ -221,16 +218,18 @@ leaves EmptyT = []
            then elementoArbol a : leaves (unArbolDeOtroArbol a)
            else leaves (unArbolDeOtroArbol a) -}
 
-leaves (NodeT x a1 a2) = if not (esArbolVacio a1 && esArbolVacio a2)
-                         then leaves 
-                         else x : leaves 
+leaves (NodeT a a1 a2) = if esArbolVacio a1 && esArbolVacio a2
+                         then a : leaves a1 ++ leaves a2
+                         else leaves a1 ++ leaves a2
 
 
 
 esHoja :: Tree a -> Bool
 -- Una hoja es un arbol cuyos arboles son vacios. Indica si el arbol dado es una hoja
-esHoja (NodeT _ EmptyT EmptyT) = True
+--esHoja (NodeT _ EmptyT EmptyT) = True
+--esHoja _ = False
 esHoja _ = False
+esHoja(NodeT _ a1 a2) = esArbolVacio a1 && esArbolVacio a2
 
 {-unArbolDeOtroArbol :: Tree a -> Tree a 
 --Dado un arbol, si alguno de sus arboles hijos es un arbol vacio, devuelve el arbol que no lo sea. Si ambos arboles son vacios, devuelve arbol vacio
@@ -241,13 +240,19 @@ unArbolDeOtroArbol (NodeT _ a1 a2) = NodeT _ a1 a2 -}
 
 --7
 heightT :: Tree a -> Int
---Dado un árbol devuelve su altura
-heightT (NodeT _ a1 a2) = if profundidadArbol1 a1 > profundidadArbol2 a2
-                          then profundidadArbol1 a1
-                          else profundidadArbol2 a2
+--Dado un árbol devuelve su altura maxima
+heightT (NodeT _ a1 a2) = if profundidadArbol a1 > profundidadArbol a2
+                          then profundidadArbol a1
+                          else profundidadArbol a2
 
 -----------------------------------------------------------
-profundidadArbol1 :: Tree a -> Int
+profundidadArbol :: Tree a -> Int
+profundidadArbol EmptyT = 0
+profundidadArbol (NodeT _ a1 a2) = --unoSiCeroSino(not (esArbolVacio a1) || not (esArbolVacio a2)) + profundidadArbol a1 + profundidadArbol a2
+                                   --unoSiCeroSino(not (esArbolVacio a1) || esHoja a2 || not (esArbolVacio a2) || esHoja a1) + profundidadArbol a1 + profundidadArbol a2
+                                     unoSiCeroSino(esHoja a1 || esHoja a2 || not (esArbolVacio a2) || not (esArbolVacio a1)) + profundidadArbol a1 + profundidadArbol a2 
+
+{-profundidadArbol1 :: Tree a -> Int
 profundidadArbol1 EmptyT = 0
 --profundidadArbol1 (NodeT _ a1 _) =
 profundidadArbol1 a = if not (esArbolVacio a)
@@ -273,7 +278,7 @@ profundidadArbol2 a = if not (esArbolVacio a)
     
                                    {-if esArbolVacio a2
                                    then profundidadArbol1 (unArbolDeOtroArbol a2) + profundidadArbol2 (unArbolDeOtroArbol a2)
-                                   else 1 + profundidadArbol2 (unArbolDeOtroArbol a2)-}
+                                   else 1 + profundidadArbol2 (unArbolDeOtroArbol a2)-}-}
 -------------------------------------------------------------                         
 esArbolVacio :: Tree a -> Bool
 --Indica si el arbol dado es vacio
@@ -294,7 +299,7 @@ segundoArbol :: Tree a -> Tree a
 segundoArbol (NodeT _ _ a2) = a2 
 
 elementoArbol :: Tree a -> a  
-elementoArbol(NodeT x _ _ ) = x
+elementoArbol(NodeT a _ _ ) = a
 
 
 --8
@@ -302,11 +307,11 @@ mirrorT :: Tree a -> Tree a
 --Dado un árbol devuelve el árbol resultante de intercambiar el hijo izquierdo con el derecho,
 --en cada nodo del árbol
 mirrorT EmptyT = EmptyT
-mirrorT (NodeT x a1 a2) = NodeT x (mirrorT (invertirArboles a2)) (mirrorT(invertirArboles a1)) -- x no la uso 
+mirrorT (NodeT a a1 a2) = NodeT a (mirrorT (invertirArboles a2)) (mirrorT(invertirArboles a1)) -- x no la uso 
 
 invertirArboles :: Tree a -> Tree a
 invertirArboles EmptyT = EmptyT
-invertirArboles (NodeT x a1 a2) = NodeT x a2 a1 -- x no la uso
+invertirArboles (NodeT a a1 a2) = NodeT a a2 a1 -- x no la uso
 
 --9
 toList :: Tree a -> [a]
@@ -314,33 +319,169 @@ toList :: Tree a -> [a]
 --Nota: En el modo in-order primero se procesan los elementos del hijo izquierdo, luego la raiz
 --y luego los elementos del hijo derecho
 --toList EmptyT = []
-toList (NodeT x a1 a2) =  elementosDelArbol a1 ++ [x] ++ elementosDelArbol a2
+toList (NodeT a a1 a2) =  elementosDelArbol a1 ++ [a] ++ elementosDelArbol a2
 
 elementosDelArbol :: Tree a -> [a]
 elementosDelArbol EmptyT = []
-elementosDelArbol (NodeT x a1 a2) = x : elementosDelArbol a1 ++ elementosDelArbol a2
+elementosDelArbol (NodeT a a1 a2) = a : elementosDelArbol a1 ++ elementosDelArbol a2
 
 --10
---levelN :: Int -> Tree a -> [a]
+levelN :: Int -> Tree a -> [a]
 {-Dados un número n y un árbol devuelve una lista con los nodos de nivel n. El nivel de un
 nodo es la distancia que hay de la raíz hasta él. La distancia de la raiz a sí misma es 0, y la
 distancia de la raiz a uno de sus hijos es 1.
 Nota: El primer nivel de un árbol (su raíz) es 0.-}
-{-levelN...............
-toList n a = if n > 0
-             then toList (n - 1) a
-             else toList-}
+levelN _ EmptyT = []
+levelN 0 (NodeT a _ _) = [a]
+levelN n (NodeT a a1 a2) = if n > 0
+                           then levelN (n - 1) a1 ++ levelN (n - 1) a2 
+                           else a : levelN n a1 ++ levelN n a2 
+ 
 
-elementoADistanciaNDeRaiz :: Int -> Tree a -> a 
+{-elementosADistanciaNDeRaiz :: Int -> Tree a -> [a] 
 --Devuelve el elemento que este en el nodo que esta a una distancia n de la raiz
-elementoADistanciaNDeRaiz _ EmptyT = error "No hay elemento"
-elementoADistanciaNDeRaiz n a = if n > 0
-                                then elementoADistanciaNDeRaiz (n - 1) (unArbolDeOtroArbol a) 
-                                else elementoArbol a
+elementosADistanciaNDeRaiz _ EmptyT = []
+elementosADistanciaNDeRaiz  0 (NodeT x _ _) = [x]
+elementosADistanciaNDeRaiz  n (NodeT x a1 a2) = if n > 0
+                                              then  elementosADistanciaNDeRaiz  (n - 1) a1 ++ elementosADistanciaNDeRaiz  (n - 1) a2 
+                                              else x : elementosADistanciaNDeRaiz  n a1 ++ elementosADistanciaNDeRaiz  n a2 -}
+
+
+--11
+
+listPerLevel :: Tree a -> [[a]]
+--Dado un árbol devuelve una lista de listas en la que cada elemento representa un nivel de
+--dicho árbol.
+listPerLevel EmptyT = []
+listPerLevel (NodeT a a1 a2) =  [a] : unirListasDeListas (listPerLevel a1) (listPerLevel a2)
+
+unirListasDeListas :: [[a]] -> [[a]] -> [[a]]
+--Dados dos listas de listas, retorna una lista de listas con todos los elementos de la primer y segunda lista juntos
+unirListasDeListas [] ys = ys
+unirListasDeListas xs [] = xs
+unirListasDeListas (x:xs) (y:ys) = (x ++ y) : unirListasDeListas xs ys
+
+
+--12
+ramaMasLarga :: Tree a -> [a]
+--Devuelve los elementos de la rama más larga del árbol
+ramaMasLarga EmptyT = []
+ramaMasLarga (NodeT _ t1 t2) = if profundidadArbol t1 > profundidadArbol t2
+                               then elementosDeUnaRama t1 -- .... ramaMasLarga t1 .... ramaMasLarga t2
+                               else elementosDeUnaRama t2
+ 
+
+elementosDeUnaRama :: Tree a -> [a]
+elementosDeUnaRama EmptyT = []
+elementosDeUnaRama (NodeT a t1 t2) = a : elementosDeUnaRama t1 ++ elementosDeUnaRama t2
+
+--12
+todosLosCaminos :: Tree a -> [[a]]
+--Dado un árbol devuelve todos los caminos, es decir, los caminos desde la raiz hasta las hojas.
+--todosLosCaminos EmptyT = []
+todosLosCaminos(NodeT a t1 t2) = [arbolALista2 t1] ++ [arbolALista2 t2]
+                                            --unirListasDeListas (arbolALista2 (todosLosCaminos t1)) (arbolALista2 (todosLosCaminos t2))
+                                            --todosLosCaminos (arbolALista2 t1) ++ todosLosCaminos (arbolALista t2)
+
+
+{-arbolALista :: Tree a -> [Tree a]
+arbolALista EmptyT = []
+arbolALista (NodeT a t1 t2) = if not (esArbolVacio t1)
+                               then t1 : arbolALista t2
+                               else arbolALista t1 ++ arbolALista t2-}
+
+arbolALista2 :: Tree a -> [a]
+arbolALista2 EmptyT = []
+arbolALista2 (NodeT a t1 t2) = if not (esHoja t1) || not (esHoja t2) 
+                               then a : arbolALista2 t1 ++ arbolALista2 t2
+                               else arbolALista2 t1 ++ arbolALista2 t2
+
+{-caminoARaices :: Tree a -> [[a]]
+caminoARaices EmptyT = []
+caminoARaices (NodeT a t1 t2) = [[a]] ++ caminoARaices t1 ++ caminoARaices t2-}
+
+                            {-Expresiones Aritméticas-}
+data ExpA = Valor Int | Sum ExpA ExpA | Prod ExpA ExpA | Neg ExpA deriving Show
+
+ejemploAritmetica :: ExpA 
+ejemploAritmetica = Prod (Prod (Valor 2) (Valor 3)) (Prod (Valor 2) (Valor 3))
+
+ejemploAritmetica2 :: ExpA
+ejemploAritmetica2 =  Prod (Valor 2) (Valor 3)
+    --Prod (Prod (Valor 2) (Valor 3)) (Prod (Valor 2) (Valor 3))
+
+ejemploAritmetica3:: ExpA
+ejemploAritmetica3 = Neg (Valor (negate(-2)))
+ 
+--1
+eval :: ExpA -> Int
+--Dada una expresión aritmética devuelve el resultado evaluarla.
+eval (Prod x y) = valorEnteroDe(accederAlValorProd x) * valorEnteroDe(accederAlValorProd y)  --valor(Valor(accederAlValor x)) * valor(Valor (accederAlValor y)) / valor(Prod(accederAlValor x) (accederAlValor y))
+eval (Sum x y) = valorEnteroDe(accederAlValorSum x) + valorEnteroDe(accederAlValorSum y)
+eval (Neg x) = valorEnteroDe(accederAlValorNeg x)
+eval _ = error "No se puede operar con un solo numero"
+--eval (Sum (Valor x) (Valor y)) = x + y
+--eval (Neg (Valor x)) = (-1) * x
+--eval (Valor x) = x
+
+accederAlValorProd:: ExpA -> ExpA
+accederAlValorProd(Prod x y) = if esUnNumero x && esUnNumero y
+                               then Valor (valorEnteroDe x * valorEnteroDe y)
+                               else accederAlValorProd (Prod (accederAlValorProd x)(accederAlValorProd y))
+                                   
+                                   --accederAlValorProd (Prod (accederAlValor x) (accederAlValor y))
+                                   --accederAlValorProd((accederAlValor x)(accederAlValor y))
+accederAlValorSum :: ExpA -> ExpA
+accederAlValorSum (Sum (Valor 0) y) = y
+accederAlValorSum (Sum x (Valor 0)) = x
+accederAlValorSum (Sum x y) =  if esUnNumero x && esUnNumero y
+                               then Valor (valorEnteroDe x + valorEnteroDe y)
+                               else accederAlValorSum(Sum (accederAlValorSum x)(accederAlValorSum y))
 
 
 
+accederAlValorNeg :: ExpA -> ExpA 
+accederAlValorNeg (Neg x) = if esUnNumero x 
+                            then Valor((-1) * valorEnteroDe x)
+                            else accederAlValorNeg (Neg (accederAlValorNeg x)) 
+
+{-accederAlValor :: ExpA -> ExpA
+accederAlValor (Prod x y) = Prod (accederAlValorProd x)(accederAlValorProd x)-} -- <- necesito mejorar esta funcion
 
 
+esUnNumero :: ExpA -> Bool
+--Dado un dato de tipo ExpA, indica si este es un valor
+{-esUnNumero (Prod _ _) = False
+esUnNumero (Sum _ _) = False
+esUnNumero (Neg _) = False-}
+esUnNumero (Valor _) = True
+esUnNumero _ = False
 
+valorEnteroDe :: ExpA -> Int
+--Funcion observadora de Valor
+valorEnteroDe (Valor x) = x
+
+esProducto :: ExpA -> Bool
+--Dado un dato de tipo ExpA, indica si este es un producto
+esProducto (Prod _ _) = True
+esProducto _ = False
+
+
+simplificar :: ExpA -> ExpA
+--Dada una expresión aritmética, la simplifica según los siguientes criterios (descritos utilizando
+--notación matemática convencional):
+ {- a) 0 + x = x + 0 = x
+    b) 0 * x = x * 0 = 0
+    c) 1 * x = x * 1 = x
+    d) - (- x) = x  -}
+
+simplificar (Sum (Valor 0) y) = y
+simplificar (Sum x (Valor 0)) = x
+simplificar (Prod (Valor 1) y) = y
+simplificar (Prod x (Valor 1)) = x
+--simplificar (Neg (Valor (x < 0))) = valorEnteroDe x * (-1)
+simplificar(Neg (Valor x)) = if x < 0
+                             then Valor (x * (1))
+                             else Valor x
+simplificar _ = error "No se puede simplificar"
 
